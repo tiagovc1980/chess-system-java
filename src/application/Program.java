7415b594a537ca70d97bfa9fb4a4ef006a09a05f
Tiago VC <vc.tiago@gmail.com>
2022-12-26 10:13:58 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Position p = new Position (5, 8);
+		System.out.println(p);
 
 	}
 
